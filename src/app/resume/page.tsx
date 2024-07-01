21dc9023ee38d3e9b21f5b12c725fecb8324ee65
TobBot2 <trevoroblack@gmail.com>
2024-07-01 19:11:52 -0400

    apossssss;

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,8 @@
-
-
 export default function ResumePdfViewer() {
     return (
         <div className="flex flex-grow w-full md:w-5/6">
             <object data="/resume.pdf" type="application/pdf" className="flex flex-auto">
-                <p>Resume PDF failed to load. Email me at trevoroblack@gmail.com, and I'll send it to you!</p>
+                <p>Resume PDF failed to load. Email me at trevoroblack@gmail.com, and I&apos;ll send it to you!</p>
             </object>
         </div>
     )
